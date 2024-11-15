1ea9ca77ff098fcf358fc369f3011cc6c1863bfe
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-11-14 19:30:45 -0500

    added get score

---
+++ game1/get_score.php
@@ -0,0 +1,37 @@
+<?php
+header("Content-Type: application/json");
+$mysqli = new mysqli("localhost", "root", "", "game1");
+if ($mysqli->connect_error)
+{
+    echo json_encode(["message" => "error"]);
+}
+else
+{
+    $username = $_POST["username"];
+
+
+    $sql = "SELECT score FROM users WHERE username=?;";
+    $query = $mysqli->prepare($sql);
+    $query->bind_param("s", $username);
+    $query->execute();
+    $result = $query->get_result();
+    if ($result->num_rows > 0)
+    {
+        $score = $result->fetch_assoc()["score"];
+
+        echo json_encode([
+            "message" => "success",
+            "score" => $score
+        ]);
+    }
+    else
+    {
+        echo json_encode([
+            "message" => "error",
+            "score" => 0
+        ]);
+    }
+
+
+    $mysqli->close();
+}
